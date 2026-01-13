9520def810e743d124d47700ee6b6c008f9e29b2
CIITSHSDTP00\CIIT <114934334+Yuf0ria@users.noreply.github.com>
2026-01-13 13:26:36 +0800

    Yippee;l

--- 2nd_Week/Assets/AgentAI.cs
+++ 2nd_Week/Assets/AgentAI.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Rendering;
 
 public class AgentAI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private NavMeshAgent agent;
+    public Transform pointA, pointB, currentTarget;
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(pointA.position);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (agent.pathPending) return;
+        if (agent.remainingDistance <= agent.stoppingDistance && agent.velocity.sqrMagnitude == 0)
+        {
+           currentTarget = (currentTarget == pointA) ? pointB : pointA;
+            agent.SetDestination(currentTarget.position);
+        }
     }
 }
