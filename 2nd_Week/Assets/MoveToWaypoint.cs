a8b6c088613db0a41b24b80bcc7bf2027d78109c
CIITSHSDTP00\CIIT <114934334+Yuf0ria@users.noreply.github.com>
2026-01-13 12:08:50 +0800

    Error;

---
+++ 2nd_Week/Assets/MoveToWaypoint.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class MoveToWaypoint : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform target;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+    }
+}
+
